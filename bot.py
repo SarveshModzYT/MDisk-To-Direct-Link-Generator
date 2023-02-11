3263b2199ec5a5b4faafa68ffa4d912be0537bf8
SarveshModzYT <124548285+SarveshModzYT@users.noreply.github.com>
2023-02-11 08:45:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ from pyrogram import Client, filters
 from helper_funcs.helpers import humanbytes, convert
 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5850007371:AAGSBwQLHyJSOoUR9zcdmLZ28ukdb96UNnQ")
 
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "24535490"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "93601197223ea651e1eeaf2a60d741c4")
 
 
 app = Client("mdisk", bot_token=TG_BOT_TOKEN, api_hash=API_HASH, api_id=APP_ID)
